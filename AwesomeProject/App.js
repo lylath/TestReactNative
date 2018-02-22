3f43a11e9b9255b2eac1f408e8d2c0539cf900d0
Lylath <nicolas-meneux@orange.fr>
2018-02-22 10:52:34 +0100

    Yay

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.intro}>Bienvenue sur BeHere !</Text>
+        <Text style={styles.intro}>Vous allez découvrir ici une multitude de breuvages plus incroyables les uns que les autres !</Text>
+        <Text style={styles.intro}>Êtes vous prêts ? XoXo</Text>
       </View>
     );
   }
@@ -20,4 +20,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  intro: {
+    color: 'red',
+    textAlign: 'center',
+  }
 });
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => App);
